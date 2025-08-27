1d5884950feeeac860458c6e691f1a2b2e9b1db0
hallejr <126441805+hallejr@users.noreply.github.com>
2025-08-27 05:33:14 -0600

    Update and rename Items/ExampleItem.cs to CoffeePendant.cs

---
+++ CoffeePendant.cs
@@ -0,0 +1,14 @@
+﻿using COTL_API.CustomInventory;
+using System.IO;
+
+namespace CotLTemplateMod.Items
+{
+    internal class ExampleItem : CustomInventoryItem
+    {
+        public override string InternalName => "Coffee_Pendant";
+        public override string LocalizedName() { return "Coffee Pendant"; }
+        public override string LocalizedDescription() { return "All speed, no sleep."; }
+
+        public override Sprite InventoryIcon => TextureHelper.CreateSpriteFromPath(Path.Combine(Plugin.PluginPath, "Assets", "coffee_necklace.png"));
+    }
+}
